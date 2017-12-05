f53f4233c0815321d9b6d080d2224f5501a92ffc
Gennady Portnov <33938041+GD-Portnov@users.noreply.github.com>
2017-12-05 22:43:19 +0300

    switch case

--- HW_01/Source.cpp
+++ HW_01/Source.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+int number;
+
 float a = 0;
 float b = 0;
 float c = 0;
@@ -14,34 +16,42 @@ float usd = 0.037;
 
 int main()
 {
-	cout << "Enter A:\n";
-	cin >> a;
-	cout << "Enter B:\n";
-	cin >> b;
-	c = a + b;
-	cout << "Sum of A + B = " << c<<"\n";
+	cout << "Please enter number of example\n";
+	cout << "1. - A + B =\n";
+	cout << "2. - (A + B)/2 =\n";
+	cout << "3. - Quotation of UAH in RUR,EUR,USD\n";
+	cout << "4. -\n";
+	cin >> number;
 
-	cout << "Enter A:\n";
-	cin >> a;
-	cout << "Enter B:\n";
-	cin >> b;
-	c = (a + b) / 2;
-	cout << "(A + B)/2 = " << c<<"\n";
-	//test
-	cout << "Enter Amount of Grivna:\n";
-	cin >> a;
-	b = a*rur;
-	c = a*eur;
-	d = a*usd;
-	cout << "RUR=" << b << "\nEUR=" << c << "\nUSD=" << d<<"\n";
-	//test2
+	switch (number)
+	{
+	case 1:
+		cout << "Enter A:\n";
+		cin >> a;
+		cout << "Enter B:\n";
+		cin >> b;
+		c = a + b;
+		cout << "Sum of A + B = " << c << "\n";
+		break;
+	case 2:
+		cout << "Enter A:\n";
+		cin >> a;
+		cout << "Enter B:\n";
+		cin >> b;
+		c = (a + b) / 2;
+		cout << "(A + B)/2 = " << c << "\n";
+		break;
+	case 3:
+		cout << "Enter Amount of Grivna:\n";
+		cin >> a;
+		b = a*rur;
+		c = a*eur;
+		d = a*usd;
+		cout << "RUR=" << b << "\nEUR=" << c << "\nUSD=" << d << "\n";
+		break;
+	}
 
-	cout << "Enter Amount of Grivna:\n";
-	cin >> a;
-	b = a*rur;
-	c = a*eur;
-	d = a*usd;
-	cout << "RUR=" << b << "\nEUR=" << c << "\nUSD=" << d << "\n";
+	
 
 	cin >> a;
 
